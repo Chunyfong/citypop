53847d68d63f6f443994ca6b7e8c3857f2417da2
Kata <50175081+Chunyfong@users.noreply.github.com>
2023-04-06 11:36:39 +0800

    Update song_prep.py

--- song_prep.py
+++ song_prep.py
@@ -8,7 +8,19 @@ kpop = r'C:\Users\\Desktop\dataset\songs\aespa.wav'
 signal, sr = librosa.load(kpop)
 librosa.display.waveshow(signal, sr=sr)
 
+# plt.xlabel("Time")
+# plt.ylabel("Amplitude")
+# plt.show()
 
-plt.xlabel("Time")
-plt.ylabel("Amplitude")
+
+fft = np.fft.fft(signal)
+magnitude = np.abs(fft)
+frequency = np.linspace(0, sr, len(magnitude))
+
+left_freq = frequency[:int(len(frequency)/2)]
+left_mag = magnitude[:int(len(frequency)/2)]
+
+plt.plot(left_freq, left_mag)
+plt.xlabel("Frequency")
+plt.ylabel("Magnitude")
 plt.show()
